5e720629e68a41a9862beaeb74b6c9b2f3e93a6f
Grey22 <klsammie24@gmail.com>
2021-02-17 13:39:53 +0100

    correction

--- controller/auth.js
+++ controller/auth.js
@@ -47,7 +47,7 @@ exports.register= async (req, res) => {
   let user = await User.findOne({email:req.body.email})
   
   if(user) return res.status(400).json({success:false,msg:"User with this email already exist"})
-    const user = await  User.create(req.body)   
+     user = await  User.create(req.body)   
      sendTokenResponse(user,200,res)
   } 
  
